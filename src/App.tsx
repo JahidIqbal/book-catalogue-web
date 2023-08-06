beb75c166cf0e5ee1e7ce1cb1bb63b3f3428ecff
hrid620 <hridishere@gmail.com>
2023-08-06 18:36:06 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// App.tsx
+import React from 'react'; // Add this line to import React
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="mt-4">
+      <footer className="bg-light text-center text-lg-start">
+
+        <div className="text-center p-3">
+          © 2023 Copyright:
+          <a className="text-dark" href="https://mdbootstrap.com/">
+            Bookstore.com
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
